dac949d9f1a73ca22ef1e14996d9cd142b9a3c4b
Yrrieth <thierry.wen@laposte.net>
2021-01-04 20:54:49 +0100

    Ajout étoile de notation

--- front/pages/bd.php
+++ front/pages/bd.php
@@ -62,10 +62,10 @@
                             
                     </div>
 
-                    <div class="my-4 d-flex flex-column" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-                        <div class="block">
-                            <div class="row">
-                                <div class="col-10">
+                    <div class="container my-4 d-flex flex-column" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                        <div class="row">
+                            <div class="block col-9">
+                                <div class="">
                                     <h3>Sypnosis</h3>
                                     <?php 
                                         echo $_SESSION['description'];
@@ -73,23 +73,84 @@
                                     <hr>
                                     <h5>Genre</h5>
                                 </div>
-                                <div class="px-0 col-8">
-                                    
-                                </div>
+                                
                             </div>
 
-                            <div class="row">
-                                <!--<style>
-                                    var star = document.getElementsByClassName('')
-                                </style>-->
-                                <span class="fa fa-star checked"></span>
-                                <span class="fa fa-star checked"></span>
-                                <span class="fa fa-star checked"></span>
-                                <span class="fa fa-star"></span>
-                                <span class="fa fa-star"></span>
+                            <div class="block mx-auto column">
+                                <div class="flex-column mb-5 column">
+                                    <div>
+                                        <h5>Note</h5>
+                                        <h1>5/5</h1>
+                                        <div id="starRating">
+                                            <span class="fa fa-star" data-star="1"></span>
+                                            <span class="fa fa-star" data-star="2"></span>
+                                            <span class="fa fa-star" data-star="3"></span>
+                                            <span class="fa fa-star" data-star="4"></span>
+                                            <span class="fa fa-star" data-star="5"></span>
+                                        </div>
+                                        <script type="text/javascript">
+                                            //https://gomakethings.com/showing-star-based-ratings-on-hover-or-focus-with-vanilla-javascript/
+                                            var starRating = document.getElementById("starRating");
+                                            var stars = Array.from(document.getElementsByClassName("fa-star"));
+                                            var star; // Current star clicked
+                                            var isClicked = false;
+                                            var clickedValue;
+                                            var isConnected;
+
+                                            if (<?php echo isset($_SESSION['id']) && isset($_SESSION['email']) ? 'true' : 'false';?>) {
+                                                isConnected = true;
+                                            } else {
+                                                isConnected = false;
+                                            }
+                                            
+                                            starRating.addEventListener("click", function(event) {
+                                                if (!isConnected) {
+                                                    window.location.href = "inscription.php";
+                                                }
+
+                                                star = event.target.closest('.fa-star');
+                                                
+                                                stars.forEach(function(element) {
+                                                    element.style.border = null;
+
+                                                })
+                                                star.style.border = "solid";
+                                                isClicked = true;
+                                                clickedValue = star.getAttribute("data-star");
+                                            });
+
+                                            starRating.addEventListener("mouseover", function(event){
+                                                star = event.target.closest('.fa-star');
+                                            
+                                                stars.forEach(function(element) {
+                                                    if (element.getAttribute("data-star") <= star.getAttribute("data-star")) {
+                                                        element.classList.add("checked");
+                                                    } else {
+                                                        element.classList.remove("checked");
+                                                    }
+                                                })
+                                            });
+
+                                            starRating.addEventListener("mouseout", function(event){    
+                                                stars.forEach(function(element) {
+                                                    if (!isClicked) {
+                                                        element.classList.remove("checked");
+                                                    } else {
+                                                        if (element.getAttribute("data-star") <= clickedValue) {
+                                                            element.classList.add("checked");
+                                                        } else {
+                                                            element.classList.remove("checked");
+                                                        }
+                                                    }
+                                                })
+                                            })
+                                        </script>
+                                    </div>
+                                </div>
+
+                                <div class="btn btn-danger">Je le possède !</div>
                             </div>
-                            
-                        
+
                         </div>
                     </div>
                     
